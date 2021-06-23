318b6b977572ced28882ba1f26439d2356bced49
pthomas0309 <preston.thomas355@gmail.com>
2021-06-23 15:43:19 -0500

    fix route

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -10,7 +10,7 @@ import { useHistory } from 'react-router-dom';
     }
     return (
       <div className="container">
-        <p onClick={navigateAdd}>Navigate me to ADD ITEM</p>
+        <p onClick={navigateAdd}>Add an Item</p>
         <h2>Shelf</h2>
         <p>All of the available items can be seen here.</p>
       </div>
