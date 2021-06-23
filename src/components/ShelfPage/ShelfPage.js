78f87d3ff5c3bae184c0e04ec448a3415bd384f8
Ryan Maas <rdmjobs@live.com>
2021-06-23 15:49:21 -0500

    Added useHistory

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,6 +1,7 @@
 // ⬇ Dependent functionality:
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function ShelfPage() {
 
